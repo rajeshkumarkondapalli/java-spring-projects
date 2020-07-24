e79133a2cf5a86b6d806c47d9de78dfa0364825b
Rajesh Kumar Kondapalli <rajesh@Rajeshs-MacBook-Pro.local>
2020-07-24 09:03:43 +0530

    update

--- project-template/src/main/java/com/sample/MainApplication.java
+++ project-template/src/main/java/com/sample/MainApplication.java
@@ -1,13 +1,13 @@
-package com.test.mailparser;
+package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MailParserApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MailParserApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
--- word-parser/src/main/java/com/sample/MainRestController.java
+++ word-parser/src/main/java/com/sample/MainRestController.java
@@ -1,4 +1,4 @@
-package com.test.mailparser;
+package com.sample;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MailRestController {
+public class MainRestController {
     @GetMapping("/ping")
     public String ping() {
-        return "ping";
+        return "pong";
     }
 
     @PostMapping(path = "/parse")
