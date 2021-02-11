62ab8a2d48766291776ceebe58fda7a81237b3d7
rajesh <rajesh@rajeshs-MacBook-Pro.local>
2021-02-11 11:16:47 +0530

    updated

---
+++ spring-project-template/src/main/java/com/project/MainRestController.java
@@ -0,0 +1,20 @@
+package com.project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainRestController {
+	@GetMapping("/ping")
+	public String ping() {
+		return "ping";
+	}
+
+	@PostMapping(path = "/parse")
+	public String addMember(@RequestBody String value) {
+		return value;
+	}
+
+}
