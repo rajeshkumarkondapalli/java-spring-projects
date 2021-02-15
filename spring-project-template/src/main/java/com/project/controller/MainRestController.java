756f682dbcef2450718a3194eb69db9f9bfa5fe2
rajesh <rajesh@rajeshs-MacBook-Pro.local>
2021-02-15 15:51:41 +0530

    updated

--- spring-project-template/src/main/java/com/project/controller/MainRestController.java
+++ spring-project-template/src/main/java/com/project/controller/MainRestController.java
@@ -1,4 +1,4 @@
-package com.project;
+package com.project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
---
+++ spring-project-template/src/main/java/com/project/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.project.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.project.model.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+	List<Customer> findByLastName(String lastName);
+
+	Customer findById(long id);
+}
