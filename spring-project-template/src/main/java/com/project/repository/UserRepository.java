9c1f23aa8708f235d6622d8ce0d71f0826be3dcb
Rajesh Kumar Kondapalli <6193256+rajeshkumarkondapalli@users.noreply.github.com>
2022-03-16 12:06:18 -0400

    Delete spring-project-template directory

--- spring-project-template/src/main/java/com/project/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.project.model.*;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
